d3389f942cb0147ee5de5203f9664f7bac2643d6
Robert Rojas Fernández <rrojas@brangerbriz.com>
2019-10-23 09:46:22 -0500

    Update PokemonStats.js

--- app/src/components/elements/PokemonStats/PokemonStats.js
+++ app/src/components/elements/PokemonStats/PokemonStats.js
@@ -6,11 +6,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import "./PokemonStats.css"
 
 const PokemonStats = stats => {
-  console.log(stats, stats.length)
-
-  const strToComponent = stats => stats.map(stat => <div>Hola</div>)
-
-  return <div className="pokedex-pokemon-stats">{strToComponent(stats)}</div>
+  return <div className="pokedex-pokemon-stats">Print Stats</div>
 }
 
 export default PokemonStats
